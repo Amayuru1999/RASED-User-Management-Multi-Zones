b9972b74755b9b6614abc160b1979cf6e7b6e5e5
Amayuru1999 <amarasingheau@gmail.com>
2026-05-13 15:33:31 +0530

    fix: rendering UIs on client side

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,13 +1,14 @@
-'use client'
-
 import { SharedAppLayout } from 'rased-shared-ui'
-import { useAuthStore } from '../store/authStore'
+import { getIronSession } from 'iron-session'
+import { cookies } from 'next/headers'
+import { SessionData, SESSION_OPTIONS } from '../lib/session'
 
 const BASE_PATH = '/users'
 
-export function AppLayout({ children }: { children: React.ReactNode }) {
-  const { user } = useAuthStore()
-  const userName = user ? `${user.firstName} ${user.lastName}` : 'test User'
+export async function AppLayout({ children }: { children: React.ReactNode }) {
+  const cookieStore = await cookies()
+  const session = await getIronSession<SessionData>(cookieStore, SESSION_OPTIONS)
+  const userName = session.user ? `${session.user.firstName} ${session.user.lastName}` : 'test User'
 
   return (
     <SharedAppLayout userName={userName} logoutHref={`${BASE_PATH}/api/auth/logout`}>
